1ac069831cf4c45fd31e0562ed8c05262cb0fb96
Tom Dallimore <me@tomdallimore.com>
2020-12-01 00:07:40 +0700

    Updated error message for oauth via quickmail API.

--- lib/quickmail/authentication.rb
+++ lib/quickmail/authentication.rb
@@ -11,14 +11,8 @@ module Quickmail
                                   payload: payload,
                                   headers: {content_type: "application/x-www-form-urlencoded"}
                                 }).execute do |response, request, result|
-          if response.code != 200
-            raise ApiRequestError.new(
-              response_code: response.code,
-              response_headers: response.headers,
-              response_body: response.to_str
-            )
-          end
-          response
+          str_response = response.to_str
+          str_response.blank? ? '' : JSON.parse(str_response)
         end
       end
     end
